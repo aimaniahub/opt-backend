375b5c21ade17f707f22d6fcc0d5a7e877489dd2
aimaniahub <aimania.hub@gmail.com>
2025-03-13 20:12:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -474,4 +474,5 @@ def get_stock_sector(symbol: str) -> str:
     return sector_mapping.get(symbol, 'Others')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT variable
+    app.run(host='0.0.0.0', port=port)
